eb7118f5a56d62be1175ea966d6fc906308cae6c
paiva73 <mau.paiva10@gmail.com>
2024-07-08 05:22:11 -0300

    feat: agrega controladores de volumen y btn para ver reglas
    Botón de reglas, funcionalidad a desarrollar.

---
+++ frontend/src/components/footer/opciones/SliderVolume.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+import styles from '../Footer.module.css';
+
+const SliderVolume = ({ onChange, value }) => {
+  return (
+    <Box className={styles.handleVolume}>
+      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+        <VolumeDown />
+        <Slider
+          aria-label="Volume"
+          value={value}
+          onChange={onChange}
+          min={0}
+          max={1}
+          step={0.001}
+        />
+        <VolumeUp />
+      </Stack>
+    </Box>
+  );
+};
+
+export default SliderVolume;
